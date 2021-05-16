b0039967719a0c603ee3b77a0fbe700274fe7e19
Aminata48 <aminata.camara.2@ulaval.ca>
2021-05-16 14:58:08 -0400

    Vuetify added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 // import VueSlider from 'vue-slider-component'
 import ProgressBar from 'vuejs-progress-bar'
+
+import vuetify from './plugins/vuetify'
 Vue.use(ProgressBar);
 //Vue.config.productionTip = false
 require("./scss/main.scss");
 // Vue.component('VueSlideBar', VueSlider)
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
